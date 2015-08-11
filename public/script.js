3d231b5a1a34d85428310826ffa743e21b4258c6
Clement <clem83f@hotmail.fr>
2015-08-11 19:32:54 +0200

    Added the Google Map Api and a basic HTML/CSS structure

---
+++ public/script.js
@@ -0,0 +1,18 @@
+window.onload = loadScript;
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD1GV7nRU-6pq2_DZJklc2BXRSrwsy2O5E&callback=initialize';
+  document.body.appendChild(script);
+}
+
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(43.4331, 6.7372)
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
--- starter.js
+++ starter.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var morgan = require('morgan');
 var debug = require('debug')('server:global');
 var server = require('http').createServer(app);
@@ -7,6 +8,11 @@ var io = require('socket.io')(server);
 
 app.use(morgan('dev'));
 
+app.use(express.static('public'));
+
+app.get('/*', function(req, res){
+  res.status(404).redirect('/');
+})
 
 server.listen(3000);
 server.on('listening', function(){
